7eadc490d0ecb5a0fa8752d69540c6e22f5f9e0d
John P. Dickerson <john@cs.umd.edu>
2019-02-21 15:34:21 -0500

    P1, P2, P4 shells

---
+++ src/hspc2019/p1/CountingWithDash_solution.java
@@ -0,0 +1,53 @@
+package hspc2019.p1;
+
+import java.util.Scanner;
+
+public class CountingWithDash_solution {
+
+   private static boolean isPrime(int num) {
+      if (num == 2) return true;
+      if (num%2 == 0) return false;
+      for (int i=3; i<=Math.sqrt(num); i+=2) {
+         if (num%i == 0) return false;
+      }
+      return true;
+   }
+   private static boolean isSquare(int num) {
+      return (Math.pow((int)(Math.sqrt(num)),2) == num);
+   }
+
+   private static String speakForDash(Scanner sc) {
+   //YOUR CODE HERE
+      String answer = "";
+      int start = sc.nextInt();
+      int end = sc.nextInt();
+      for (int i=start; i<=end; i++) {
+         boolean prime = isPrime(i);
+         boolean square = isSquare(i);
+         if (prime) answer+="Prime";
+         else if (square) answer+="Square";
+         else answer+=i;
+      }
+      return answer;
+   //YOUR CODE HERE
+   }
+
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int numOfTestCases = sc.nextInt();
+      sc.nextLine();
+
+      for (int testCase=0; testCase < numOfTestCases; testCase++) { 
+         System.out.println(
+            "Dash says: " + speakForDash(sc)
+         );
+      }
+      
+      sc.close();
+   }
+
+
+}
+
---
+++ src/hspc2019/p2/CountingWithJackJack_solution.java
@@ -0,0 +1,43 @@
+package hspc2019.p2;
+
+import java.util.Scanner;
+
+public class CountingWithJackJack_solution {
+
+   private static String speakForJackJack(Scanner sc) {
+   //YOUR CODE HERE
+      String answer = "";
+      int start = sc.nextInt();
+      int end = sc.nextInt();
+      for (int i=start; i<=end; i++) {
+         boolean divBy4 = (i%4)==0;
+         boolean divBy5 = (i%5)==0;
+         if (!divBy4 && !divBy5) answer+=i;
+         else {
+            if (divBy4) answer+="Fam";
+            if (divBy5) answer+="Fro";
+         }
+      }
+      return answer;
+   //YOUR CODE HERE
+   }
+
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int numOfTestCases = sc.nextInt();
+      sc.nextLine();
+
+      for (int testCase=0; testCase < numOfTestCases; testCase++) { 
+         System.out.println(
+            "JackJack says: " + speakForJackJack(sc)
+         );
+      }
+      
+      sc.close();
+   }
+
+
+}
+
