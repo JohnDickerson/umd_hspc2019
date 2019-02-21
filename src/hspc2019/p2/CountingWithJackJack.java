79ee53c9f1377e4bf520b210299ceeee9b837081
John P. Dickerson <john@cs.umd.edu>
2019-02-21 16:35:22 -0500

    P2 Test Input/Output

---
+++ src/hspc2019/p2/CountingWithJackJack.java
@@ -0,0 +1,31 @@
+package hspc2019.p2;
+
+import java.util.Scanner;
+
+public class CountingWithJackJack {
+
+	private static String speakForJackJack(Scanner sc) {
+		//YOUR CODE HERE
+		return "";
+		//YOUR CODE HERE
+	}
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int numOfTestCases = sc.nextInt();
+		sc.nextLine();
+
+		for (int testCase=0; testCase < numOfTestCases; testCase++) { 
+			System.out.println(
+					"JackJack says: " + speakForJackJack(sc)
+					);
+		}
+
+		sc.close();
+	}
+
+
+}
+
